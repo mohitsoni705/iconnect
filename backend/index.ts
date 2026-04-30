4b67f8af574e2bacd5c8a2bf1867c1127e66f54f
mohitsoni705 <mohitravet07@gmail.com>
2026-04-30 21:54:24 +0530

    signin routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,24 +1,33 @@
-import express from "express";
-import jwt from "jsonwebtoken";
-import cors from "cors";
-import dotenv from "dotenv";
-import z from "zod";
-import { Socket } from "socket.io";
+    import express from "express";
+    import jwt from "jsonwebtoken";
+    import cors from "cors";
+    import dotenv from "dotenv";
+    import z from "zod";
+    import { Socket } from "socket.io";
+    import { pool } from "./db/db";
+    const app = express();
+    app.use(express.json);
+    dotenv.config();
 
-
-const app = express();
-app.use(express.json);
-dotenv.config();
-
-app.post("/",(req,res)=>{
-    res.json({
-        "message":"Bybybyyy"
+    app.post("/",(req,res)=>{
+        res.json({
+            "message":"Bybybyyy"
+        })
     })
-})
 
-app.post("/signup",(req,res)=>{
-    
-})
-app.listen(8000,()=>{
-    console.log("Server started at 8000");
-})
+    app.post("/signup",async(req,res)=>{
+        const {username , password , phonenumber , email } = req.body;
+        const response = await pool.query(
+            `INSERT INTO users VALUE (
+            ${username} , ${phonenumber} , ${email} , ${password} )`
+        )
+
+    })
+    app.get("/signin",async(req,res)=>{
+        const {phonenumber , password} = req.body;
+        const response = await pool.query(`
+            SELECT `)
+    })
+    app.listen(8000,()=>{
+        console.log("Server started at 8000");
+    })
